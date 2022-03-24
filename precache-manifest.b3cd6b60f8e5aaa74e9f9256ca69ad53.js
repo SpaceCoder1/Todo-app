9c116c950c54969f983855627bf092d864441029
Abhishek Ruby <88341600+SpaceCoder1@users.noreply.github.com>
2022-03-24 10:16:44 +0530

    Add files via upload

---
+++ precache-manifest.b3cd6b60f8e5aaa74e9f9256ca69ad53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4721009057932d304f05",
+    "url": "/Todo-app/static/css/main.829c17e4.chunk.css"
+  },
+  {
+    "revision": "4721009057932d304f05",
+    "url": "/Todo-app/static/js/main.47210090.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/Todo-app/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "1b9852ba5058245a8866",
+    "url": "/Todo-app/static/js/2.1b9852ba.chunk.js"
+  },
+  {
+    "revision": "9046afbd380c4e72cc9f",
+    "url": "/Todo-app/static/js/runtime~main.9046afbd.js"
+  },
+  {
+    "revision": "761a94a5c6f7fb0656cb46933a7d3f55",
+    "url": "/Todo-app/index.html"
+  }
+];
